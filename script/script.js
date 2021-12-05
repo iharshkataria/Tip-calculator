3dbe845e03f5bf53da283e71a83a1d36545d3247
Harsh kataria <katariaharsh0001@gmail.com>
2021-12-06 03:19:00 +0530

    folder create

--- script/script.js
+++ script/script.js
@@ -3,5 +3,5 @@ btn.addEventListener('click',function(){
     let billAmount = document.getElementById('bill-amount').value;
     let tipPercentage = document.getElementById('tip-percentage').value;
     let tipAmount = document.getElementById('tip-amount').value = (billAmount/100)*tipPercentage;
-    document.getElementById('total-bill').value=parseFloat(billAmount) + parseFloat(tipAmount);
+    alert(document.getElementById('total-bill').value=parseFloat(billAmount) + parseFloat(tipAmount));
 })
